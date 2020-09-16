a01b8f2cac262e36a12fd80c7d156eb4db8066b7
suprimnakarmi <nanjel14@gmail.com>
2020-09-16 15:01:48 +0545

    Used CacheDataset to load and transform the dataset; Also loaded the dataset
    CacheDataset was used for both the training and validation dataset; Batch size of 10 was allocated for training dataset and batch size of 1 for validation dataset

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import monai
 import matplotlib.pyplot as plt
 import os
 import glob
+from monai.data import CacheDataset, DataLoader, Dataset
+
 import dataset
 import data_transformation
 
@@ -18,3 +20,17 @@ labels = dataset.load_data_path(label_path)
 # Zipping the image and label to load it
 data_dicts = [{"image": image, "label": label} for image,label in zip(images, labels)]
 train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+
+# Transforming the dataset
+train_ds = CacheDataset(data=train_files,
+                        transform=data_transformation.train_transforms,
+                        cache_rate=1.0,
+                        num_workers=4)
+val_ds = CacheDataset(data=val_files,
+                      transform=data_transformation.val_transforms,
+                      cache_rate=1.0,
+                      num_workers=4)
+
+# Loading the dataset
+train_loader = DataLoader(train_ds, batch_size= 10, shuffle= True, num_workers=4)
+val_loader= DataLoader(val_ds, batch_size = 1)

fa15cc083e479af8eeabfd07069be2294d3b07a2
suprimnakarmi <nanjel14@gmail.com>
2020-09-16 17:11:04 +0545

    Added train accuracy

--- training.py
+++ training.py
@@ -1,6 +1,9 @@
-from models import model_unet
+import torch
+from models import model_unet, device
+from main import optimizer, loss_function
 
-def train():
+
+def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds ):
     best_metric = -1
     best_metric_epoch = -1
     total_train = 0
@@ -20,9 +23,18 @@ def train():
                 batch_data["label"].to(device),
             )
             optimizer.zero_grad()
-            outputs=model_unet(inputs)
+            outputs =model_unet(inputs)
             loss = loss_function(ouputs,labels)
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
             epoch_len = len(train_ds)// train_loader.batch_size
+
+            # Check the accuracy for training dataset
+            _, predicted = torch.max(outputs.data,1)
+            total_train += labels.nelement()
+            correct_train += predicted.eq(torch.squeeze(labels.data,1)).sum().item()
+            train_accuracy = correct_train/ total_train
+
+            print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}, train_accuracy: {train_accuracy}")
+            

00c6c50ed31a42755617a74c72b51ca0527ae63a
suprimnakarmi <nanjel14@gmail.com>
2020-09-30 08:24:28 +0545

    Replaced the object of ResUnet with the object of SegNet to train

--- training.py
+++ training.py
@@ -4,7 +4,7 @@ from monai.inferers import sliding_window_inference
 from monai.metrics import compute_meandice
 
 
-from models import model_unet, device
+from models import model_unet, device, SegNet
 #from main import optimizer, loss_function
 
 
@@ -15,10 +15,11 @@ def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds, l
     metric_sum_train = 0.0
     epoch_loss_values = list()
     metric_values = list()
+    net = SegNet(1,2)
     for epoch in range(epoch_num):
         print("-"*10)
         print(f"epoch {epoch+1}/{epoch_num}")
-        model_unet.train()
+        net.train()
         epoch_loss=0
         step = 0
         for batch_data in train_loader:
@@ -28,7 +29,7 @@ def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds, l
                 batch_data["label"].to(device),
             )
             optimizer.zero_grad()
-            outputs = model_unet(inputs)
+            outputs = net(inputs).to(device)
             loss = loss_function(outputs,labels)
             loss.backward()
             optimizer.step()
@@ -61,7 +62,7 @@ def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds, l
         writer.add_scalar("train_metric", metric_train, epoch + 1)
 
         if(epoch+1)% val_interval==0:  # Evaluation of the model
-            model_unet.eval()
+            net.eval()
             metric_sum=0.0
             metric_count= 0
             for val_data in val_loader:
@@ -70,14 +71,14 @@ def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds, l
                     val_data["label"].to(device),
                 )
                 # Calculating the validation loss
-                v_outputs= model_unet(val_inputs)
+                v_outputs= net(val_inputs)
                 loss_val = loss_function(v_outputs, val_labels)
                 print(f"validation_loss: {loss_val.item()}")
                 writer.add_scalar("validation_loss", loss_val.item(), epoch_len* epoch + step)
 
                 roi_size=(160,160)
                 sw_batch_size= 4
-                val_outputs = sliding_window_inference(val_inputs,roi_size, sw_batch_size, model_unet)
+                val_outputs = sliding_window_inference(val_inputs,roi_size, sw_batch_size, net)
                 # Computing mean dice for evaluation
                 value = compute_meandice(
                     y_pred = val_outputs,
@@ -94,7 +95,7 @@ def train(epoch_num, val_interval, train_loader, val_loader, writer, train_ds, l
             if metric>best_metric:
                 best_metric = metric
                 best_metric_epoch = epoch +1
-                torch.save(model_unet.state_dict(), os.path.join('/home/suprim/dataset/MontgomerySet/best_metric',"best_metric_model_10.pth"))
+                torch.save(net.state_dict(), os.path.join('/home/suprim/dataset/MontgomerySet/best_metric',"best_metric_model_10.pth"))
                 print("saved new best metric model")
                 print(f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
                       f"\n best mean dice: {best_metric:.4f} at epoch: {best_metric_epoch}"
